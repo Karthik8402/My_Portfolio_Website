8df0f4d905eff987c53baf4acd272a30f73f5b4d
Karthik <124026006+Karthik8402@users.noreply.github.com>
2023-05-17 14:30:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let tablinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname, event){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(let tabcontent of tabcontents){
+    tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+    }
