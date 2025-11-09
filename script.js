8597157c657baea6ff54c73fa6a21a0d742004ef
karthik8402 <karthikumar8402@gmail.com>
2025-11-09 16:40:46 +0530

    remove the confict and saved

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*-------------------Showing Condent---------------------*/ 
 let tablinks = document.getElementsByClassName("tab-links");
         let tabcontents = document.getElementsByClassName("tab-contents");
@@ -13,17 +12,3 @@ let tablinks = document.getElementsByClassName("tab-links");
         event.currentTarget.classList.add("active-link");
         document.getElementById(tabname).classList.add("active-tab");
         }
-=======
-let tablinks = document.getElementsByClassName("tab-links");
-let tabcontents = document.getElementsByClassName("tab-contents");
-function opentab(tabname, event){
-    for(let tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(let tabcontent of tabcontents){
-    tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-}
->>>>>>> f1ffdae44d17bfb7998215baf6ba58261fe38785
